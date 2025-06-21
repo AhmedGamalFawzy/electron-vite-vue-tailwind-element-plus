993aa0e4c9ce47e2b592de108dcb0484415128a5
Jimmy <ahmed.gamal@brightskiesinc.com>
2025-06-21 22:09:25 +0300

    Initialize Electron application with Vue and TypeScript setup. Add ESLint and Prettier configurations for code quality, and set up Tailwind CSS for styling.

---
+++ electron.vite.config.ts
@@ -0,0 +1,31 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
+})
